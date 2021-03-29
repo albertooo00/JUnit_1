8125c6f49f060e2e956d1f29891519fce219278a
Albertooo Safin <sultanofsicness@icloud.com>
2021-03-29 15:56:25 +0300

    добавил случай для незарегистрированого пользователя над лимитом

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -61,5 +61,20 @@ class BonusServiceTest {
         long actual = service.calculate(amount, registered);
         assertEquals(expected, actual);
     }
+    
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForDonTRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
 
+        // подготавливаем данные:
+        long amount = 1_000_000_060;
+        boolean registered = false;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
 }
