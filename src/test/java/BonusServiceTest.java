f42a8e34917688a8edef73a941cc09ebcd57b5c9
Albertooo Safin <sultanofsicness@icloud.com>
2021-04-07 19:20:54 +0300

    добавил лимит в третий случай

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -54,7 +54,7 @@ class BonusServiceTest {
     void shouldCalculateForDonTRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
 
-        long amount = 1_000_000_600;
+        long amount = 1_000_000_060;
         boolean registered = true;
         long expected = 500;
 
