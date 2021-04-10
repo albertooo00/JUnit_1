70b11e95f654afc36a7a40603c2b497fa871729b
Albertooo Safin <sultanofsicness@icloud.com>
2021-04-10 13:00:13 +0300

    1000_60

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -54,7 +54,7 @@ class BonusServiceTest {
     void shouldCalculateForDonTRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
 
-        long amount = 1_000_000_060;
+        long amount = 1000_60;
         boolean registered = true;
         long expected = 500;
 
