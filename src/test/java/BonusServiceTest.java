dc5970858fc6e3e7fc3e17f3f77b6f84397163e3
Albertooo Safin <sultanofsicness@icloud.com>
2021-04-12 12:40:09 +0500

    поменял ожидаемый с 30 на 10, поменял амаунт, теперь базовый тест проходит, я проверил

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -54,9 +54,9 @@ class BonusServiceTest {
     void shouldCalculateForDonTRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
 
-        long amount = 1_000_000_60;
-        boolean registered = true;
-        long expected = 500;
+        long amount = 1000_60;
+        boolean registered = false;
+        long expected = 10;
 
         long actual = service.calculate(amount, registered);
         assertEquals(expected, actual);
